a7d8cbec9b8662ba23e7868a1de4f0ec6e7dd9ed
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2017-03-17 00:28:08 +0200

    added changes to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HeaderComponent, NavigationComponent, FooterComponent],
-  exports: [HeaderComponent, NavigationComponent, FooterComponent]
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  declarations: [
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
+  ],
+  providers: []
 })
 
 export class CoreModule {
