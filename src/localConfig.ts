675e1fef7ed6cc2baffab95f08f0a868cce77abe
Nazar Tsyhanyuk <Nazartsyhanyuk@gmail.com>
2017-03-26 20:00:29 +0300

    Delete localConfig.ts

--- src/localConfig.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  GOOGLE_API: {
-    CLIENT_ID: '178749370794-e564lj6pnsb244vjpa83gpnjvrhc65e0.apps.googleusercontent.com',
-    COOKIE_POLICY: 'single_host_origin'
-  }
-};
